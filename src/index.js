52ee6b9b3d341fd86a6429d4db339e4d9c25b7c0
Samuelisch <samuelchanwk@hotmail.com>
2021-07-21 21:43:57 +0800

    added template path to HtmlWebpackPlugin, for output to dist/index.html

--- src/index.js
+++ src/index.js
@@ -18,3 +18,45 @@ View task comment / details / description
 */
 
 //localStorage - stores all projects and in those projects, all todos
+
+//Factory function for tasks
+const CreateTask = (title = 'New task', description = '', due = 'Today') => {
+    //change title, description or date
+    function setTitle(title) {
+        this.title = title;
+    }
+
+    function setDescription(description) {
+        this.description = description;
+    }
+
+    function setDue(due) {
+        this.due = due;
+    }
+
+    return {title, description, due, setTitle, setDescription, setDue};
+};
+
+//store tasks into an array - a list of tasks - project
+//create Project factory function
+const CreateProject = (title = 'New project') => {
+    //initialise project array
+    let projectList = [];
+
+    //function to set title
+    function setTitle(title) {
+        this.title = title;
+    };
+
+    //adds tasks to current projectList
+    function addTask(task) {
+        projectList.push(task);
+    };
+
+    return {title, setTitle, projectList, addTask};
+}
+
+const task1 = CreateTask('get groceries', 'description', 'tomorrow');
+const project1 = CreateProject('daily');
+project1.addTask(task1);
+console.log(project1.projectList);
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
           title: 'To-do list',
+          template: './src/index.html',
         }),
     ],
     output: {
