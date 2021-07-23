c0b7f6cc51c0c702d86ae9084c2270997dcc9f72
Samuelisch <samuelchanwk@hotmail.com>
2021-07-23 18:28:57 +0800

    create dir modules - added all module files into directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import project from './project.js';
-import task from './task.js';
-import storage from './storage.js';
-import UI from './UI.js';
+import project from './modules/project.js';
+import task from './modules/task.js';
+import storage from './modules/storage.js';
+import UI from './modules/UI.js';
 import './style.css';
 
 UI.addSiteBehaviour();
